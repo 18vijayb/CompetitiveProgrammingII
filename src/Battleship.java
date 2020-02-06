1fc1fb47f86d13ad2ffa22ba0470a0fecbe2d2de
18vijayb <18vijayb@students.harker.org>
2020-02-06 18:28:56 -0500

    Solved CarnivalHopscotch DP Problem

--- src/Battleship.java
+++ src/Battleship.java
@@ -6,6 +6,36 @@ public class Battleship {
         {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             int numCases = Integer.parseInt(br.readLine());
+            for (int i=0; i<numCases; i++)
+            {
+                StringTokenizer currentLine = new StringTokenizer(br.readLine());
+                int width = Integer.parseInt(currentLine.nextToken());
+                int height = Integer.parseInt(currentLine.nextToken());
+                int numShots = Integer.parseInt(currentLine.nextToken());
+                boolean[][] playerOne = new boolean[width][height];
+                boolean[][] playerTwo = new boolean[width][height];
+                int playerOneNumHashtags = 0;
+                int playerTwoNumHashtags = 0;
+                for (int j=0; j<height; j++)
+                {
+                    String shipLine = br.readLine();
+                    for (int k=0; k<width; k++) {
+                        playerOne[j][k]=shipLine.charAt(k)=='#';
+                        playerOneNumHashtags++;
+                    }
+                }
+                for (int j=0; j<height; j++) {
+                    String shipLine = br.readLine();
+                    for (int k = 0; k < width; k++) {
+                        playerTwo[j][k] = shipLine.charAt(k) == '#';
+                        playerTwoNumHashtags++;
+                    }
+                }
+                for (int j=0; j<numShots; j++)
+                {
+
+                }
+            }
         }
         catch (Exception e)
         {
---
+++ src/CarnivalHopscotch.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class CarnivalHopscotch {
+
+    public static void main(String[] args) {
+        try
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer currentLine = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(currentLine.nextToken());
+            int D = Integer.parseInt(currentLine.nextToken());
+            int[] points = new int[N];
+            for (int i=0; i<N; i++)
+            {
+                points[i]=Integer.parseInt(br.readLine());
+            }
+            int leastPoints=0;
+            int[] leastPointsSequence = new int[D];
+            for (int i=0; i<N; i++)
+            {
+                leastPoints = Integer.MAX_VALUE;
+                for (int j=0; j<D; j++)
+                {
+                    if (i-j>=0)
+                        leastPoints = Math.min(leastPoints,leastPointsSequence[(i-j)%D]);
+                }
+                leastPointsSequence[i%D]=leastPoints+points[i];
+                //System.out.println("For square " + i + " the least number of points possible is " + (leastPoints+points[i]));
+            }
+            //System.out.println(Arrays.toString(leastPointsSequence));
+            leastPoints = Integer.MAX_VALUE;
+            for (int i=0; i<D; i++)
+                leastPoints = leastPoints<leastPointsSequence[i] ? leastPoints : leastPointsSequence[i];
+            System.out.println(leastPoints);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
