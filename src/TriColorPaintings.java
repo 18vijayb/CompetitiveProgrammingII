919b3c156a2d214b959d12ef9961d19a9e4eaf8c
18vijayb <18vijayb@students.harker.org>
2020-02-06 18:42:59 -0500

    Solved DiningCourts simulation problem.

---
+++ src/TriColorPaintings.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class TriColorPaintings {
+    public static void main(String[] args) {
+        try
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer currentLine = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(currentLine.nextToken());
+            int K = Integer.parseInt(currentLine.nextToken());
+        }
+        catch (Exception e )
+        {
+            e.printStackTrace();
+        }
+    }
+}
