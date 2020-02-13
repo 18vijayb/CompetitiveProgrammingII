7cf0f052bcfc7d6434c6ad5ee904ee4d6b5ee835
18vijayb <18vijayb@students.harker.org>
2020-02-12 23:31:39 -0500

    Solved TriColorPaintings problem

--- src/TriColorPaintings.java
+++ src/TriColorPaintings.java
@@ -10,6 +10,7 @@ public class TriColorPaintings {
             int K = Integer.parseInt(currentLine.nextToken());
             HashMap<Integer,List<Integer>> different = new HashMap<>();
             HashMap<Integer,List<Integer>> same= new HashMap<>();
+            //ALWAYS MAP BIGGER STRIPES TO SMALLER STRIPES
             for (int i=0; i<K; i++)
             {
                 currentLine = new StringTokenizer(br.readLine());
@@ -17,69 +18,88 @@ public class TriColorPaintings {
                 {
                     int stripe1 = Integer.parseInt(currentLine.nextToken())-1;
                     int stripe2 = Integer.parseInt(currentLine.nextToken())-1;
-                    if (! (same.containsKey(stripe1)))
-                    {
-                        same.put(stripe1,new ArrayList<>());
+                    if (stripe1>stripe2) {
+                        if (!(same.containsKey(stripe1))) {
+                            same.put(stripe1, new ArrayList<>());
+                        }
+                        same.get(stripe1).add(stripe2);
                     }
-                    same.get(stripe1).add(stripe2);
-                    if (! (same.containsKey(stripe2)))
-                    {
-                        same.put(stripe2,new ArrayList<>());
+                    else {
+                        if (!(same.containsKey(stripe2))) {
+                            same.put(stripe2, new ArrayList<>());
+                        }
+                        same.get(stripe2).add(stripe1);
                     }
-                    same.get(stripe2).add(stripe1);
                 }
                 else
                 {
                     int stripe1 = Integer.parseInt(currentLine.nextToken())-1;
                     int stripe2 = Integer.parseInt(currentLine.nextToken())-1;
-                    if (! (different.containsKey(stripe1)))
-                    {
-                        different.put(stripe1,new ArrayList<>());
+                    if (stripe1>stripe2) {
+                        if (!(different.containsKey(stripe1))) {
+                            different.put(stripe1, new ArrayList<>());
+                        }
+                        different.get(stripe1).add(stripe2);
                     }
-                    different.get(stripe1).add(stripe2);
-                    if (! (different.containsKey(stripe2)))
-                    {
-                        different.put(stripe2,new ArrayList<>());
-                    }
-                    different.get(stripe2).add(stripe1);
-                }
-            }
-            int[] numCombos = new int[N];
-            for (int i=0; i<N; i++)
-            {
-                numCombos[i]=3;
-            }
-            for (int i=0; i<N; i++)
-            {
-                if (same.containsKey(i))
-                {
-                    List<Integer> sameColor = same.get(i);
-                    for (int j: sameColor)
-                    {
-                        if (i<j)
-                            numCombos[j]=1;
-                    }
-                }
-                if (different.containsKey(i))
-                {
-                    List<Integer> differentColor = different.get(i);
-                    System.out.println("OKAY IM LOOKING AT THE " + (i+1) + "th stripe and the number of different color constraints on it is " + differentColor.size());
-                    for (int j: differentColor)
-                    {
-                        if (i<j && numCombos[j]>0)
-                            numCombos[j]--;
+                    else {
+                        if (!(different.containsKey(stripe2))) {
+                            different.put(stripe2, new ArrayList<>());
+                        }
+                        different.get(stripe2).add(stripe1);
                     }
                 }
             }
-            int runningMult = 1;
-            for (int i: numCombos)
-                runningMult*=i;
-            System.out.println(Arrays.toString(numCombos));
-            System.out.println(runningMult);
+            int[] currentPainting = new int[N];
+            int backTracking = rec(0,currentPainting, different, same);
+            System.out.println(backTracking);
         }
         catch (Exception e )
         {
             e.printStackTrace();
         }
+    }
+    public static int rec(int currentIndex, int[] currentPainting, HashMap<Integer,List<Integer>> different, HashMap<Integer,List<Integer>> same)
+    {
+        if (currentIndex==currentPainting.length) {
+            //System.out.println("The final painting is " + Arrays.toString(currentPainting));
+            return 1;
+        }
+        Set<Integer> availableGoodColors = new HashSet<>();
+        if (same.containsKey(currentIndex))
+        {
+            //System.out.println("Yo im at the " + currentIndex + " index");
+            List<Integer> sameList = same.get(currentIndex);
+            availableGoodColors.add(currentPainting[sameList.get(sameList.size()-1)]);
+            //System.out.println(availableGoodColors);
+        }
+        else
+        {
+            availableGoodColors.add(1);
+            availableGoodColors.add(2);
+            availableGoodColors.add(3);
+        }
+        if (different.containsKey(currentIndex))
+        {
+            List<Integer> differentList = different.get(currentIndex);
+            HashSet<Integer> cantBeTheseColors = new HashSet<>();
+            for (int i: differentList)
+                cantBeTheseColors.add(currentPainting[i]);
+            if (cantBeTheseColors.contains(1))
+                availableGoodColors.remove(1);
+
+            if (cantBeTheseColors.contains(2))
+                availableGoodColors.remove(2);
+            if (cantBeTheseColors.contains(3))
+                availableGoodColors.remove(3);
+        }
+        int numPaintings=0;
+        //System.out.println(availableGoodColors);
+        for (int color: availableGoodColors)
+        {
+            currentPainting[currentIndex] = color;
+            numPaintings+=rec(currentIndex+1,currentPainting,different,same);
+
+        }
+        return numPaintings;
     }
 }
